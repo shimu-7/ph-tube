27d0aff41ef55cd21795a862dbdc591dcad6b962
shimu-7 <shamima19shimu@gmail.com>
2023-09-02 14:28:24 +0600

    showing videos

---
+++ js/view.js
@@ -0,0 +1,77 @@
+
+const loadCategory = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/videos/categories')
+    const data = await res.json()
+    const category = data.data
+    // console.log(phones);
+    displayCategory(category);
+}
+
+const displayCategory = (categories) => {
+    const btn_container = document.getElementById('category');
+    for (const category of categories) {
+        const btn=document.createElement('a');
+        btn.innerHTML=`
+        <a class="btn sm:btn-sm md:btn-md" onclick="showCategoryVideo('${category.category_id}')">${category.category}</a>
+        `;
+        btn_container.appendChild(btn);
+    }
+
+}
+const loadCategoryVideo = async(id) =>{
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
+    const data = await res.json()
+    const video = data.data;
+    console.log(video);
+    displayCategoryVideo(video);
+
+}
+
+const displayCategoryVideo = (videos) =>{
+    const container = document.getElementById('showVideoContainer');
+    container.textContent = '';
+
+    videos.forEach(video => {
+        // console.log(phone);
+        const div = document.createElement('div');
+        div.innerHTML=`
+        <div class="card h-[350px] card-compact bg-base-100 shadow-xl">
+                <figure><img src="${video.thumbnail}" class="h-[200px]" alt="Shoes" /></figure>
+                <div class="card-body flex flex-row ">
+                    <div>
+                    <div class="avatar">
+                    <div class="w-[50px] h-[50px] rounded-full">
+                      <img src="${video.authors[0].profile_picture}" />
+                    </div>
+                  </div>
+                          
+                    </div>
+                    <div>
+                        <h2 class="card-title">${video.title}</h2>
+                        
+                        <div class="flex flex-row">
+                        <p id="author_name">${video.authors[0].profile_name}</p>
+    
+
+                        </div>
+                        <p>${video.others.views} Views</p>
+                    </div>
+                </div>
+            </div>
+        `;
+        container.appendChild(div);
+        // list=document.getElementById('author_name');
+        // div=document.createElement('div').innerHTML=`<i class="fa-solid fa-check"></i>`;
+        // list.appendChild(div);
+    });
+
+
+    
+    
+}
+const showCategoryVideo = (id) =>{
+    loadCategoryVideo(id);
+}
+loadCategory();
+loadCategoryVideo(1000);
